e12dfa3e4a62408333c25bd1cf9bc8200fcbe51b
Simon Brown <simonandon@live.com.au>
2024-06-26 12:13:06 +0800

    implemented socket.io

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,10 @@ import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 
 import connectToMongoDB from './db/connectToMongoDB.js';
+import { app, server } from './socket/socket.js';
 
-const app = express();
+// moved to socket/socket.js
+// const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -24,7 +26,8 @@ app.use('/api/users', userRoutes);
 // 	res.send('Hello world');
 // });
 
-app.listen(PORT, () => {
+// socket.io server set up in socket.js
+server.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server running on port ${PORT}`);
 });
--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -6,12 +6,13 @@ const Message = ({ message }) => {
 	const { authUser } = useAuthContext();
 	const { selectedConversation } = useConversation();
 	const fromMe = message.senderId === authUser._id;
-	const formattedTime = extractTime(message.createdAt)
+	const formattedTime = extractTime(message.createdAt);
 	const chatClassName = fromMe ? 'chat-end' : 'chat-start';
 	const profilePic = fromMe
 		? authUser.profilePic
 		: selectedConversation?.profilePic;
 	const bubbleBgColor = fromMe ? 'bg-blue-500' : '';
+	const shakeClass = message.shouldShake ? 'shake' : '';
 
 	return (
 		<div className={`chat ${chatClassName}`}>
@@ -20,7 +21,9 @@ const Message = ({ message }) => {
 					<img src={profilePic} alt="avatar image" />
 				</div>
 			</div>
-			<div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>
+			<div
+				className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}
+			>
 				{message.message}
 			</div>
 			<div className="chat-footer opacity-50 text-xs flex gap-1 items-center pb-2">
