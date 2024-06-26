f1ddf8e8fb715e6f0f5e6e3fc566e220005a90d5
Simon Brown <simonandon@live.com.au>
2024-06-26 13:19:30 +0800

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
@@ -13,6 +14,8 @@ import { app, server } from './socket/socket.js';
 // const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -22,9 +25,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
-// app.get('/', (req, res) => {
-// 	res.send('Hello world');
-// });
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 // socket.io server set up in socket.js
 server.listen(PORT, () => {
